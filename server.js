e986d022a15ea15cb902b756d7bee48b2a444686
Ceci007 <cecibot9@gmail.com>
2022-03-21 11:43:19 -0300

    Adding a schema

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { graphqlHTTP } = require('express-graphql');
+const schema = require("./schema/schema");
 
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}));
+
 app.listen(4000, () => {
-    console.log("listening!");
+    console.log("listening at http://localhost:4000/graphql");
 });
