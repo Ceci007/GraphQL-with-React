34f7803f200a04c3ee4044223b755ac98616730c
Ceci007 <cecibot9@gmail.com>
2022-03-22 11:08:15 -0300

    Asociate UserType with a company

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,29 @@ const {
     GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: "User",
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
+        company: { 
+            type: CompanyType, 
+            resolve(parentValue, args) {
+                console.log(parentValue, args);
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(res => res.data);
+            }
+        }
     }
 });
 
@@ -23,7 +40,8 @@ const RootQuery = new GraphQLObjectType({
             type: UserType, 
             args: { id: { type: GraphQLString }},
             resolve(parentValue, args) {
-                return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data);
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                .then(res => res.data);
             },
         }
     }
